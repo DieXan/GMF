6143229d698eb24f6b057284d897256a2b3ad5b9
DieXan <ilshatabdullin2003@gmail.com>
2022-10-07 20:15:37 +0300

    я гений

--- GMF/Program.cs
+++ GMF/Program.cs
@@ -1,14 +1,51 @@
 ﻿using System;
+using Telegram.Bot;
 using working_with_db;
+using System.Threading;
+using Telegram.Bot.Types;
 using working_with_scrapper;
+using System.Threading.Tasks;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
 class main
 {
+
+    static ITelegramBotClient bot = new TelegramBotClient("1925272046:AAF_kH6jfBY1B53L_EUTRhTBR_IsR1f5Ph0");
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+        if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+        {
+            var message = update.Message;
+            if (message.Text.ToLower() == "/start")
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Здравствуйте!");
+                return;
+            }
+            await botClient.SendTextMessageAsync(message.Chat, "Дорогой, я ничего не понимаю!");
+        }
+    }
+    public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+    }
     static void Main()
     {
-        //db db = new db();
-        //db.create_db();
-        //first first = new first();
-        //first.get_one();
+        Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { }, // receive all update types
+        };
+        bot.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            receiverOptions,
+            cancellationToken
+        );
+        Console.ReadLine();
         Console.Read();
     }
 }
