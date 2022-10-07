013a3e3f8a4e1265cec86cc6f26f094cd7d44462
DieXan <ilshatabdullin2003@gmail.com>
2022-10-07 19:33:13 +0300

    starting work with parser

--- GMF/Program.cs
+++ GMF/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using Microsoft.Data.Sqlite;
 using working_with_db;
+using working_with_scrapper;
 class main
 {
     static void Main()
     {
-        db db = new db();
-        db.create_db();
+        //db db = new db();
+        //db.create_db();
+        first first = new first();
+        first.get_one();
         Console.Read();
     }
 }
---
+++ working_with_scrapper/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace working_with_scrapper
+{
+    public class first
+    {
+        public void get_one()
+        {
+            string htmlCode;
+            using (WebClient client = new WebClient())
+            {
+                htmlCode = client.DownloadString("http://burgerking-menu-i-ceny.ru/");
+            }
+            string[] s1 = htmlCode.Split("<h2 class=\"text-center mt-4 mb-3 text-uppercase\">");
+            for (int i = 1; i < s1.Length; i++)
+            {
+                string[] s2 = s1[i].Split("</h2>");
+                Console.WriteLine(s2[0]);
+            }
+        }
+    }
+}
