c499c161b696f3fc93c44d7574ca51e37ea225dd
DieXan <ilshatabdullin2003@gmail.com>
2022-10-07 20:04:40 +0300

    recode

--- working_with_telegram/Class1.cs
+++ working_with_telegram/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace working_with_telegram
-{
-    public class Class1
-    {
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
 
+namespace working_with_telegram
+{
+    public class telegram_hehe
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("TOKEN");
+        public void send_message()
+        {
+            botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+        }
     }
 }
