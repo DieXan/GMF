f98ea429ab065b54c2cbc7d6bb1cc543d3d9bc4a
DieXan <ilshatabdullin2003@gmail.com>
2022-12-17 11:02:27 +0300

    а

--- OrderHandler/OrderHandler.cs
+++ OrderHandler/OrderHandler.cs
@@ -6,28 +6,32 @@ namespace OrderHandler
     {
         public static string GetFoodForUser(int UserID)
         {
-            string str = "Ваш заказ будет составать из:\n";
             string ss = UO.GetUserOption(UserID);
             string[] s = ss.Split(":");
             int drink = Convert.ToInt32(s[0]);
             int burger = Convert.ToInt32(s[1]);
             int etc = Convert.ToInt32(s[2]);
             int balance = Convert.ToInt32(s[3]);
+            string str = $"Изначальный баланс: {s[3]}\n\nВаш заказ будет составать из:";
             if (burger == 1) 
             {
                 string[] tmp = db.FindBurger(balance).Split(":");
                 str += $"\nНазвание позиции: {tmp[0]}\nЦена: {tmp[1]}";
+                balance -= Convert.ToInt32(tmp[1]);
             }
             if (drink == 1)
             {
                 string[] tmp = db.FindDrink(balance).Split(":");
                 str += $"\nНазвание позиции: {tmp[0]}\nЦена: {tmp[1]}";
+                balance -= Convert.ToInt32(tmp[1]);
             }
             if (etc == 1)
             {
                 string[] tmp = db.FindEtc(balance).Split(":");
                 str += $"\nНазвание позиции: {tmp[0]}\nЦена: {tmp[1]}";
+                balance -= Convert.ToInt32(tmp[1]);
             }
+            str += $"\n\nОкончательный баланс: {Convert.ToString(balance)}";
             return str;
         }
     }
